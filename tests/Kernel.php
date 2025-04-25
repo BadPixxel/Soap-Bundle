fce26cd664484232a020fe65a62287061450e9f6
BadPixxel <eshop.bpaquier@gmail.com>
2025-04-25 23:33:41 +0200

    Add test Kernel for Php-Soap-Bundle
    Introduced a custom test Kernel to support testing in the Php-Soap-Bundle. This Kernel uses the MicroKernelTrait and sets the configuration directory for test purposes.

---
+++ tests/Kernel.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ *  This file is part of SplashSync Project.
+ *
+ *  Copyright (C) Splash Sync  <www.splashsync.com>
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ *  For the full copyright and license information, please view the LICENSE
+ *  file that was distributed with this source code.
+ */
+
+namespace Splash\Connectors\Soap\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\HttpKernel\Kernel as BaseKernel;
+
+class Kernel extends BaseKernel
+{
+    use MicroKernelTrait;
+
+    /**
+     * Gets the path to the configuration directory.
+     */
+    private function getConfigDir(): string
+    {
+        return $this->getProjectDir().'/tests/config';
+    }
+}
